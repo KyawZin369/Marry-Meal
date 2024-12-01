d99b2f80a58f68c83618dad7454d37b363c346a7
KhuSoe <sawebenzerpozan@gmail.com>
2024-12-01 22:18:29 +0700

    FIx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 
-import Form  from "@/components/Form";
+import Form from "@/components/Form";
 import { useSearchParams } from "next/navigation";
 
-export default function Registeration () {
+export default function Registeration() {
+  const searchParams = useSearchParams();
+  const role = searchParams.get("role") || "member";
 
-    const searchParams = useSearchParams();
-    const role = searchParams.get('role') || "member";
-
-
-    return (
-        <div>
-            <Form roleType={role} action={"register"}/>
-        </div>
-    );
+  return;
+  <Form roleType={role} action={"register"} />;
 }
